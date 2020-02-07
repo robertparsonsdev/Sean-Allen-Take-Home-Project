908226a78b8167315e3fc49013ca7d0566e22177
Robert Parsons <robertparsons4@icloud.com>
2020-02-06 19:21:32 -0800

    Created follower list view controller, pass the username from the text field of button, and added some basic text field validation

---
+++ GHFollowers/FollowerListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FollowerListViewController.swift
+//  GHFollowers
+//
+//  Created by Robert Parsons on 2/6/20.
+//  Copyright © 2020 Robert Parsons. All rights reserved.
+//
+
+import UIKit
+
+class FollowerListViewController: UIViewController {
+    var username: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
