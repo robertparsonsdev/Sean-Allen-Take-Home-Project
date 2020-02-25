9931e8e4091987d1202ff1746cfa39dc1475ec26
Robert Parsons <robertparsons4@icloud.com>
2020-02-24 16:06:43 -0800

    Now uses the Result type in NetworkManager.

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -14,24 +14,24 @@ class NetworkManager {
     
     private init() {}
     
-    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, ErrorMessage?) -> Void) {
+    func getFollowers(for username: String, page: Int, completed: @escaping (Result<[Follower], GFError>) -> Void) {
         let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
         guard let url = URL(string: endpoint) else {
-            completed(nil, .invalidUsername)
+            completed(.failure(.invalidUsername))
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             if let _ = error {
-                completed(nil, .unableToComplete)
+                completed(.failure(.unableToComplete))
                 return
             }
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(nil, .invalidResponse)
+                completed(.failure(.invalidResponse))
                 return
             }
             guard let data = data else {
-                completed(nil, .invalidData)
+                completed(.failure(.invalidData))
                 return
             }
             
@@ -39,11 +39,43 @@ class NetworkManager {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let followers = try decoder.decode([Follower].self, from: data)
-                completed(followers, nil)
+                completed(.success(followers))
             } catch {
-                completed(nil, .invalidData)
+                completed(.failure(.invalidData))
             }
         }
         task.resume()
+        
+        // -- before Swift 5 --
+//        let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
+//        guard let url = URL(string: endpoint) else {
+//            completed(nil, .invalidUsername)
+//            return
+//        }
+//
+//        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+//            if let _ = error {
+//                completed(nil, .unableToComplete)
+//                return
+//            }
+//            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+//                completed(nil, .invalidResponse)
+//                return
+//            }
+//            guard let data = data else {
+//                completed(nil, .invalidData)
+//                return
+//            }
+//
+//            do {
+//                let decoder = JSONDecoder()
+//                decoder.keyDecodingStrategy = .convertFromSnakeCase
+//                let followers = try decoder.decode([Follower].self, from: data)
+//                completed(followers, nil)
+//            } catch {
+//                completed(nil, .invalidData)
+//            }
+//        }
+//        task.resume()
     }
 }
--- GHFollowers/Screens/FollowerListViewController.swift
+++ GHFollowers/Screens/FollowerListViewController.swift
@@ -16,13 +16,11 @@ class FollowerListViewController: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad Stuff", message: errorMessage!.rawValue, buttonTitle: "Okay")
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (result) in
+            switch result {
+                case .success(let followers): print(followers)
+                case .failure(let error): self.presentGFAlertOnMainThread(title: "Bad Stuff", message: error.rawValue, buttonTitle: "Okay")
             }
-            print("Followers.count = \(followers.count)")
-            print(followers)
         }
     }
     
