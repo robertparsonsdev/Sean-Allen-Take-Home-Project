2729683e8e51ad072bf31083a68a804e495d76b9
Robert Parsons <robertparsons4@icloud.com>
2020-03-14 12:10:33 -0700

    Implemented loading view with activity indicator.

--- GHFollowers/Custom Views/GFAlertViewController.swift
+++ GHFollowers/Custom Views/GFAlertViewController.swift
@@ -98,14 +98,3 @@ class GFAlertViewController: UIViewController {
     }
 
 }
-
-extension UIViewController {
-    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
-        DispatchQueue.main.async {
-            let alertVC = GFAlertViewController(title: title, message: message, buttonTitle: buttonTitle)
-            alertVC.modalPresentationStyle = .overFullScreen
-            alertVC.modalTransitionStyle = .crossDissolve
-            self.present(alertVC, animated: true)
-        }
-    }
-}
