f666e491f88bf86156c25da2b7007c07223918a9
Robert Parsons <robertparsons4@icloud.com>
2020-11-11 20:07:53 -0800

    Adjusted spacing

--- GHFollowers/Screens/FollowerListViewController.swift
+++ GHFollowers/Screens/FollowerListViewController.swift
@@ -64,18 +64,18 @@ class FollowerListViewController: UIViewController {
             guard let self = self else { return }
             self.dismissLoadingView()
             switch result {
-                case .success(let followers):
-                    if followers.count < 100 { self.hasMoreFollowers = false }
-                    self.followers.append(contentsOf: followers)
-                    if self.followers.isEmpty {
-                        let message = "This user doesn't have any followers. Go follow them. 😍"
-                        DispatchQueue.main.async {
-                            self.showEmptyStateView(with: message, in: self.view)
-                        }
-                        return
+            case .success(let followers):
+                if followers.count < 100 { self.hasMoreFollowers = false }
+                self.followers.append(contentsOf: followers)
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers. Go follow them. 😍"
+                    DispatchQueue.main.async {
+                        self.showEmptyStateView(with: message, in: self.view)
                     }
-                    self.updateData(on: self.followers)
-                case .failure(let error): self.presentGFAlertOnMainThread(title: "Bad Stuff", message: error.rawValue, buttonTitle: "Okay")
+                    return
+                }
+                self.updateData(on: self.followers)
+            case .failure(let error): self.presentGFAlertOnMainThread(title: "Bad Stuff", message: error.rawValue, buttonTitle: "Okay")
             }
         }
     }
