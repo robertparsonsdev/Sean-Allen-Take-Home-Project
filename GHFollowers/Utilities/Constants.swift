8f3cea93f24860c982ef4421cfecbd4584edae72
Robert Parsons <robertparsons4@icloud.com>
2020-11-13 14:51:18 -0800

    Created and added the UserInfoHeaderVC.

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Robert Parsons on 11/13/20.
+//  Copyright © 2020 Robert Parsons. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
